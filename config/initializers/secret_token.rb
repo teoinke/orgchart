4bedac7f3a089ca2cc782c12d348cdc88f730aca
Teo inke <teoinke@hotmail.com>
2014-06-05 21:40:52 -0300

    Primeiro commit orgchart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orgchart::Application.config.secret_key_base = 'e5e99065b8dfdb979b55f08e9818d9b46431679b40cdb4d7f590112c2f1b758b3e46ad63a8e26be449c33b6ae321cfa3bd038df2e5768fb88462ca3cc5982c17'
